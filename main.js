71e3d43ecbe7b8956db4a5b0842b1453b56dd55d
HarryPotter1234567890 <100985749+HarryPotter1234567890@users.noreply.github.com>
2022-03-12 16:11:25 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function draw(){
+image(video, 0, 0, 300, 300);
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
